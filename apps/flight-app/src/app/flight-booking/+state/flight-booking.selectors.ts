05019e7aa80b1b5315b333215adee451a28a7ced
michael.zikes <michael.zikes@intauria.com>
2021-03-12 15:28:43 +0100

    Redux: Store

---
+++ apps/flight-app/src/app/flight-booking/+state/flight-booking.selectors.ts
@@ -0,0 +1,13 @@
+import { createFeatureSelector, createSelector } from '@ngrx/store';
+import * as fromFlightBooking from './flight-booking.reducer';
+
+export const selectFlightBookingState = createFeatureSelector<fromFlightBooking.State>(
+  fromFlightBooking.flightBookingFeatureKey
+);
+
+export const selectFlights = createSelector(
+  // Selectors:
+  selectFlightBookingState,
+  // Projector:
+  (state) => state.flights
+);
